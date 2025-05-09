d119399fcfd31c5bc731d897c52cfeb7bdaed062
MegaEmme <mikmusco@gmail.com>
2025-05-09 14:44:51 +0200

    feat: implement movie retrieval and error handling in controllers; fix router case sensitivity

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const moviesRouter = require('./routers/moviesRouter');
+
 //registro asset statici
 app.use(express.static('public'));
 //registro il body parser
 app.use(express.json());
-
+//registro la path del router
+app.use('/movies', moviesRouter);
+//attivo il server sulla porta
 app.listen(port, () => {
     console.log(`sono un server attivo sulla porta ${port}`)
 });
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,70 @@
 const connection = require('../data/db');
 //index
 function index (req,res) {
-    console.log('index')
+    const sql = 'SELECT * FROM movies'
+
+    connection.query(sql, (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                errorMessage: 'Database connection error'
+            })
+        }
+
+        res.json(results);
+    })
+
+    res.send('Index Movies')
 };
 //show
 function show (req,res) {
-    console.log('show')
+
+    const {id} = req.params;
+    const sql = `SELECT 
+                   *
+                FROM
+                    movies
+                WHERE
+                    id = ?
+    `
+
+    connection.query(sql, [id], (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                errorMessage: err.sqlMessage
+            })
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                errorMessage: 'No records found',
+                id
+            })
+        }
+
+        res.json(results[0]);
+
+        const movie = results[0];
+
+        const sql = `SELECT 
+                        *
+                    FROM
+                        reviews
+                    WHERE
+                        movie_id = ?
+        `
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) {
+                console.log(err);
+            }            
+
+            movie.reviews = results; 
+
+            res.json(movie);
+        })
+    })
 };
 
 module.exports = {index, show};
