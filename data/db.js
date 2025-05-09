ed6bda9fa63f9048c2e332e7f7645d8d4da7bb4a
MegaEmme <mikmusco@gmail.com>
2025-05-09 14:53:11 +0200

    fix: set default database user to 'root' in connection configuration

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER,
+    user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
