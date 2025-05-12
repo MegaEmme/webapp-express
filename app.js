aa289fc12eca09ed78650cc8d32e4e3419011373
MegaEmme <mikmusco@gmail.com>
2025-05-12 14:44:47 +0200

    feat: add CORS middleware and update dependencies in package.json and package-lock.json

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express = require('express');
 const app = express();
 //definisco la porta
 const port = process.env.PORT || 3000;
+//importo middleware cors
+const cors = require('cors');
 //importo il router
 const moviesRouter = require('./routers/moviesRouter');
 //importo middleware errorsHandler
 const errorsHandler = require('./middlewares/errorsHandler');
 //importo middleware notFound
 const notFound = require('./middlewares/notFound');
+//importo middleware cors
+app.use(cors({
+    origin: process.env.FE_APP
+}));
 //registro middleware asset statici
 app.use(express.static('public'));
 //registro middleware body parser
